f167bb6e4afa919cd7dcbee3ec842940c3ddb5b7
Singularity_Lee <496736912@qq.com>
2018-09-11 10:50:23 +0800

    .

--- NumberCalculate/ViewController.m
+++ NumberCalculate/ViewController.m
@@ -23,16 +23,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    UILabel *remind=[[UILabel alloc]initWithFrame:CGRectMake(50, 70, 150, 20)];
+    remind.textColor=[UIColor blackColor];
+    remind.text=@"倍数加减（2倍）";
+    [self.view addSubview:remind];
+    
     NumberCalculate *number=[[NumberCalculate alloc]initWithFrame:CGRectMake(50, 100, 150, 50)];
-    number.multipleNum=@"2";
+    number.multipleNum=@"2";//数值增减基数（倍数增减） 默认1的倍数增减
     number.minFormZero=NO;
-    number.maxNum=10;
+    number.maxNum=10;//最大值
     [self.view addSubview:number];
     number.resultNumber = ^(NSString *number) {
         NSLog(@"%@>>>resultBlock>>",number);
     };
     
     /**************************************************************/
+    UILabel *xib=[[UILabel alloc]initWithFrame:CGRectMake(50, SCREEN_HEIGHT/2-30, 200, 20)];
+    xib.textColor=[UIColor blackColor];
+    xib.text=@"xib搭建的cell上使用";
+    [self.view addSubview:xib];
+    
     
     UITableView *table=[[UITableView alloc]initWithFrame:CGRectMake(0, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT/2) style:UITableViewStylePlain];
     table.rowHeight=100;
